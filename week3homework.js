fd994542069933eda5b569dd9b6a1fc8c957b456
hoandbui01 <73191716+hoandbui01@users.noreply.github.com>
2020-11-17 13:24:31 -0600

    2nd revision

--- week3homework.js
+++ week3homework.js
@@ -1,35 +1,66 @@
-const pizzaToppings = ['cheese', 'pepperoni', 'mushroom', 'tomatoes', 'green peppers', 'pineapple'];
+const pizzaToppings = [
+  "cheese",
+  "pepperoni",
+  "mushroom",
+  "tomatoes",
+  "green peppers",
+  "pineapple",
+];
 
 // function greetCustomer() {
 //     console.log = `Welcome to Pizza House, our toppings are ${pizzaToppings}`
 // }
 
 function greetCustomer() {
-    const greeting = `Welcome to Pizza House, our toppings are ${pizzaToppings}`
-    console.log(greeting);
+  const greeting = `Welcome to Pizza House, our toppings are ${pizzaToppings}`;
+  console.log(greeting);
 }
 
 function getPizzaOrder(size, crust, pizzaToppings) {
-    console.log('Your pizza will be coming up shortly!');
-    console.log(`One ${size} ${crust} ${pizzaToppings} is your order`);
-    return [size, crust, pizzaTopping];
+  console.log("Your pizza will be coming up shortly!");
+  console.log(`One ${size} ${crust} ${pizzaToppings} is your order`);
+  return [size, crust, pizzaTopping];
 }
 
 function preparePizza([size, crust, pizzaToppings]) {
-    console.log('Cooking Pizza')
-    return {
-        size: size,
-        crust: crust,
-        toppings: pizzaToppings,
-    };
- }
+  console.log("Cooking Pizza");
+  return {
+    size: size,
+    crust: crust,
+    toppings: pizzaToppings,
+  };
+}
 
- function servePizza (pizza) {
-    console.log('Pizza is ready')
-    console.log(`Order up! Here's your ${pizza.size} ${pizza.crust} crust pizza with ${pizza.toppings}`)
-    return pizza;
- }
+function servePizza(pizza) {
+  console.log("Pizza is ready");
+  console.log(
+    `Order up! Here's your ${pizza.size} ${
+      pizza.crust
+    } crust pizza with ${listToppings(pizza.toppings)}`
+  );
+  return pizza;
+}
 
-const hoanPizza = preparePizza(['small', 'deep dish', ['cheese', 'peporoni']]);
+function listToppings(listToppings) {
+  let sentence = "";
+  for (let index = 0; index < listToppings.length; index++) {
+    const word = listToppings[index];
+    if (index === listToppings.length - 1) {
+      sentence = `${sentence}, and ${word}.`;
+    } else if (index === 0) {
+      sentence = `${word}`;
+    } else {
+      sentence = `${sentence}, ${word}`;
+    }
+  }
+  console.log(`Our toppings are ${sentence}`);
+  return sentence;
+}
+
+const hoanPizza = preparePizza([
+  "small",
+  "deep dish",
+  ["cheese", "pepporoni", "ham"],
+]);
 // console.log(hoanPizza);
 servePizza(hoanPizza);
